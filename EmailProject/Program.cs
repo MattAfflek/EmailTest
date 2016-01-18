da85c93f5e5b22444b38b50e0e9f558de6dc4096
Matt Afflek <MattAfflek@mail.ru>
2016-01-18 13:40:01 +0300

    https://github.com/MattAfflek/EmailTest.git

---
+++ EmailProject/Program.cs
@@ -0,0 +1,71 @@
+﻿using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MailKit.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailProject
+{
+    class Program
+    {
+        // Our server
+        private static string _host = "mail.s4ab.ru";
+        private static int _port = 993; // 143;
+        private static bool _secure = true; // false;
+        private static string _userName = "mail.test@as61.ru";
+        private static string _password = "Q1w2e3r4";
+
+        static void Main(string[] args)
+        {
+            Task.Run(() => MainAsync(args)).Wait();
+        }
+        static async Task MainAsync(string[] args)
+        {
+            var i = 0;
+            var max = 1000;
+            try
+            {
+                for (; i < max; i++)
+                {
+                    await Process();
+                    DisplayPercent(i / (double)max);
+                }
+                Console.WriteLine("No error?");
+                Console.WriteLine("It's strange.");
+                Console.WriteLine("Try again.");
+            }
+            catch (Exception ex)
+            {
+                var error = ex.GetBaseException();
+                Console.WriteLine("Error raise by step {0}.", i);
+                Console.WriteLine(error.Message);
+            }
+        }
+        static async Task Process()
+        {
+            using (var client = new ImapClient())
+            {
+                // Connection
+                await client.ConnectAsync(_host, _port, _secure);
+
+                // Note: since we don't have an OAuth2 token, disable
+                // the XOAUTH2 authentication mechanism.
+                client.AuthenticationMechanisms.Remove("XOAUTH");
+
+                
+                // Authentication
+                await client.AuthenticateAsync(_userName, _password);
+            }
+        }
+        static void DisplayPercent(double percent)
+        {
+            Console.Clear();
+            Console.WriteLine("{0:P2}", percent);
+        }
+    }
+}
